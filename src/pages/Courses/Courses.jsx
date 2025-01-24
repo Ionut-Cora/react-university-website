be4de2ffd8e8a38b26fb0087aa75025afb7ca90a
Ionut Cora <cora.ionut92@yahoo.com>
2025-01-24 16:15:13 +0000

    courses changes

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -15,50 +15,50 @@ const courses = [
     {
         id: 1,
         img: [ArtCourseImg],
-        title: 'Art Course',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!'
+        title: 'Arts',
+        description: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
     },
     {
         id: 2,
         img: [BusinessCourseImg],
-        title: 'Business Course',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!'
+        title: 'Business',
+        description: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
     },
     {
         id: 3,
         img: [ComputerScienceCourseImg],
-        title: 'Computer Science Course',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!'
+        title: 'Computer Science',
+        description: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.'
     },
     {
         id: 4,
         img: [EducationCourseImg],
-        title: 'Education Course',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!'
+        title: 'Education',
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
     },
     {
         id: 5,
         img: [HealthcareCourseImg],
-        title: 'Healthcare Course',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!'
+        title: 'Healthcare',
+        description: 'The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.'
     },
     {
         id: 6,
         img: [LawCourseImg],
-        title: 'Law Course',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!'
+        title: 'Law',
+        description: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy.'
     },
     {
         id: 7,
         img: [MusicCourseImg],
-        title: 'Music Course',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!'
+        title: 'Music',
+        description: 'Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
     },
     {
         id: 8,
         img: [SportCourseImg],
-        title: 'Sport Course',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!'
+        title: 'Sports',
+        description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
     },
 ];
 
@@ -77,10 +77,11 @@ function Courses() {
                 {courses.map((course) => (
                     <div key={course.id} className='col-lg-6'>
                         <Card className='text-white shadow scale-hover-effect'>
-                            <Card.Img src={course.img} />
+                            <Card.Img src={course.img} className='img-fluid' width='500' height='400' />
                             <Card.ImgOverlay className='d-flex flex-column align-items-center justify-content-center p-md-5'>
-                                <Card.Title className='fs-1 text-danger'>{course.title}</Card.Title>
+                                <Card.Title className='text-center fs-1 text-danger'>{course.title}</Card.Title>
                                 <Card.Text className='text-center'>{course.description}</Card.Text>
+                                <button type='button' className='btn btn-outline-light text-capitalize fw-semibold'>View course</button>
                             </Card.ImgOverlay>
                         </Card>
                     </div>
